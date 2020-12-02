178fbd170c1c3e05ba25322a85f58c3a25c28bc9
Alex Lockhart <alex@lockhart.dev>
2020-12-02 13:27:46 +0000

    day 2

---
+++ src/Day2.hs
@@ -0,0 +1,29 @@
+module Day2 (main, part1, part2) where
+
+import Text.Regex.PCRE
+
+main :: IO ()
+main = interact (show . \input -> (part1 input, part2 input))
+
+parse :: String -> (Int, Int, Char, String)
+parse string = do
+  let (_, _, _, min : max : char : password : _) = string =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])
+  (read min :: Int, read max :: Int, head char, password)
+
+validate1 :: (Int, Int, Char, String) -> Bool
+validate1 (min, max, char, password) = do
+  let len = length $ filter (== char) password
+  len >= min && len <= max
+
+validate2 :: (Int, Int, Char, String) -> Bool
+validate2 (min, max, char, password) = do
+  let first = password !! (min - 1)
+  let second = password !! (max - 1)
+  let len = length $ filter (== True) $ map (== char) [first, second]
+  len == 1
+
+part1 :: String -> Int
+part1 = length . filter (== True) . map (validate1 . parse) . lines
+
+part2 :: String -> Int
+part2 = length . filter (== True) . map (validate2 . parse) . lines
---
+++ test/Day2Spec.hs
@@ -0,0 +1,16 @@
+module Day2Spec (spec) where
+
+import Day2 (part1, part2)
+import Test.Hspec
+
+input = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
+
+spec :: Spec
+spec = do
+  describe "Part1" $ do
+    it ("returns 1 for " ++ input) $ do
+      part1 input `shouldBe` 2
+
+  describe "Part2" $ do
+    it ("returns 1 for " ++ input) $ do
+      part2 input `shouldBe` 1
