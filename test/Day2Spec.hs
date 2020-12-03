dd791a1f5447a1b201e06ce3302e0fea3c676ebd
Alex Lockhart <alex@lockhart.dev>
2020-12-03 12:23:41 +0000

    refactor day 2

--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -8,9 +8,9 @@ input = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
 spec :: Spec
 spec = do
   describe "Part1" $ do
-    it ("returns 1 for " ++ input) $ do
+    it ("returns 1 for\n" ++ input) $ do
       part1 input `shouldBe` 2
 
   describe "Part2" $ do
-    it ("returns 1 for " ++ input) $ do
+    it ("returns 1 for\n" ++ input) $ do
       part2 input `shouldBe` 1
