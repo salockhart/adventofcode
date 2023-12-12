858006efd2e757bcfde70ebb0c0315031ac9d39e
Alex Lockhart <alex@lockhart.dev>
2023-12-11 22:18:18 -0400

    [haskell] 2023 day 11

---
+++ adventofcode-haskell/adventofcode2023/src/Day11.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Day11 (main, part1, part2) where
+
+import AOC (CoordMap, solveAoCDay)
+import Data.List (foldl', transpose)
+import qualified Data.Map as M
+import qualified Data.Text as T
+
+main :: IO ()
+main = solveAoCDay 2023 11 part1 (part2 1000000)
+
+combinations2 :: [a] -> [[a]]
+combinations2 [] = []
+combinations2 (x : rest) = map (\y -> [x, y]) rest ++ combinations2 rest
+
+distanceBetweenPoints :: [((Int, Int), b)] -> Int
+distanceBetweenPoints [((xa, ya), _), ((xb, yb), _)] = abs (xb - xa) + abs (yb - ya)
+distanceBetweenPoints _ = error "can't get the distance, doesn't match up"
+
+parse :: Int -> [[Char]] -> [((Int, Int), Char)]
+parse emptyLinePenalty ls = do
+  let ys = getAxisPoints emptyLinePenalty ls
+  let xs = getAxisPoints emptyLinePenalty (transpose ls)
+  let m = parseIntoCoordMap' ys xs ls
+  M.toList $ M.filter (/= '.') m
+
+getAxisPoints :: Int -> [[Char]] -> [Int]
+getAxisPoints emptyLinePenalty ls = numberLines' ls [0 ..]
+  where
+    numberLines' [] _ = []
+    numberLines' (x : xs) (n : ns) =
+      n
+        : numberLines'
+          xs
+          (if not $ all (== '.') x then ns else drop (emptyLinePenalty - 1) ns)
+
+parseIntoCoordMap' :: [Int] -> [Int] -> [[Char]] -> CoordMap Char
+parseIntoCoordMap' ys xs =
+  foldl'
+    ( \m (y, line) ->
+        M.union
+          m
+          ( foldl'
+              (\m' (x, char) -> M.insert (x, y) char m')
+              M.empty
+              $ zip xs line
+          )
+    )
+    M.empty
+    . zip ys
+
+solve :: Int -> T.Text -> Int
+solve n =
+  sum
+    . map distanceBetweenPoints
+    . combinations2
+    . parse n
+    . lines
+    . T.unpack
+
+part1 :: T.Text -> Int
+part1 = solve 2
+
+part2 :: Int -> T.Text -> Int
+part2 = solve
---
+++ adventofcode-haskell/adventofcode2023/test/Day11Spec.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day11Spec (spec) where
+
+import qualified Data.Text as T
+import Day11 (part1, part2)
+import Test.Hspec
+
+input :: T.Text
+input =
+  "...#......\n\
+  \.......#..\n\
+  \#.........\n\
+  \..........\n\
+  \......#...\n\
+  \.#........\n\
+  \.........#\n\
+  \..........\n\
+  \.......#..\n\
+  \#...#....."
+
+spec :: Spec
+spec = do
+  describe "Part1" $ do
+    it ("\n" ++ T.unpack input) $ do
+      part1 input `shouldSatisfy` (== 374)
+
+  describe "Part2" $ do
+    it ("\n" ++ T.unpack input) $ do
+      part2 10 input `shouldSatisfy` (== 1030)
+      part2 100 input `shouldSatisfy` (== 8410)
