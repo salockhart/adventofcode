33802f451310ab60f25fb2f0df2f6b7358dd2182
Alex Lockhart <alex@lockhart.dev>
2024-12-05 10:27:05 -0400

    [haskell] 2024 day 05

---
+++ adventofcode-haskell/adventofcode2024/src/Day05.hs
@@ -0,0 +1,49 @@
+module Day05 (main, part1, part2) where
+
+import AOC (mkAoCMain)
+import AOC.Data.List (first2)
+import Data.Bifunctor (bimap)
+import Data.List (sortBy)
+import qualified Data.Map as M
+import qualified Data.Text as T
+
+main :: IO ()
+main = mkAoCMain 2024 05 part1 part2
+
+parse :: T.Text -> (M.Map (Int, Int) Ordering, [[Int]])
+parse =
+  bimap
+    ( M.fromList
+        . concatMap ((\t@((a, b), _) -> [t, ((b, a), GT)]) . (,LT) . first2)
+        . parseSection "|"
+    )
+    (parseSection ",")
+    . first2
+    . T.splitOn "\n\n"
+    . T.strip
+  where
+    parseSection divider =
+      map
+        ( map (read . T.unpack)
+            . T.splitOn divider
+        )
+        . T.splitOn "\n"
+
+solve :: ([Int] -> [Int] -> Bool) -> (M.Map (Int, Int) Ordering, [[Int]]) -> Int
+solve withOrdered =
+  sum
+    . map middle
+    . ( \(key, us) ->
+          map fst $
+            filter (uncurry withOrdered) $
+              map (\u -> (sort key u, u)) us
+      )
+  where
+    sort key = sortBy (\a b -> M.findWithDefault EQ (a, b) key)
+    middle xs = xs !! (length xs `div` 2)
+
+part1 :: T.Text -> Int
+part1 = solve (==) . parse
+
+part2 :: T.Text -> Int
+part2 = solve (/=) . parse
---
+++ adventofcode-haskell/adventofcode2024/test/Day05Spec.hs
@@ -0,0 +1,46 @@
+module Day05Spec (spec) where
+
+import qualified Data.Text as T
+import Day05 (part1, part2)
+import Test.Hspec
+
+input :: T.Text
+input =
+  "47|53\n\
+  \97|13\n\
+  \97|61\n\
+  \97|47\n\
+  \75|29\n\
+  \61|13\n\
+  \75|53\n\
+  \29|13\n\
+  \97|29\n\
+  \53|29\n\
+  \61|53\n\
+  \97|53\n\
+  \61|29\n\
+  \47|13\n\
+  \75|47\n\
+  \97|75\n\
+  \47|61\n\
+  \75|61\n\
+  \47|29\n\
+  \75|13\n\
+  \53|13\n\
+  \\n\
+  \75,47,61,53,29\n\
+  \97,61,53,29,13\n\
+  \75,29,13\n\
+  \75,97,47,61,53\n\
+  \61,13,29\n\
+  \97,13,75,29,47"
+
+spec :: Spec
+spec = do
+  describe "Part1" $ do
+    it ("\n" ++ T.unpack input) $ do
+      part1 input `shouldSatisfy` (== 143)
+
+  describe "Part2" $ do
+    it ("\n" ++ T.unpack input) $ do
+      part2 input `shouldSatisfy` (== 123)
